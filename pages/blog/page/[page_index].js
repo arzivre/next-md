e86c517fe083e5e00ef2ec5b5649d90ac5a0ea85
Arzivre <sonyfauzi@outlook.co.id>
2021-09-24 18:59:49 +0700

    pagination - generate path

---
+++ pages/blog/page/[page_index].js
@@ -0,0 +1,65 @@
+//@ts-check
+import fs from 'fs'
+import matter from 'gray-matter'
+import path from 'path'
+import Layout from '../../../component/Layout'
+import Post from '../../../component/Post'
+import { sortByDate } from '../../../utils'
+import { POSTS_PER_PAGE } from '../../../config'
+export default function BlogPage({ posts }) {
+  return (
+    <Layout>
+      <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
+
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join('posts'))
+
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
+
+  let paths = []
+
+  for (let i = 1; i <= numPages; i++) {
+    paths.push({
+      params: { page_index: i.toString() },
+    })
+  }
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'))
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '')
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    )
+
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter,
+    }
+  })
+
+  return {
+    props: {
+      posts: posts.sort(sortByDate),
+    },
+  }
+}
