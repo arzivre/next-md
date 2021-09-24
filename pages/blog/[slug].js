e6bf9facdd7d186b4e6c8e82cd051c772e490f20
Arzivre <sonyfauzi@outlook.co.id>
2021-09-24 18:43:10 +0700

    deploy single blog post

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,16 +1,58 @@
 //@ts-check
 import fs from 'fs'
 import path from 'path'
+import Link from 'next/link'
+import Image from 'next/image'
 import matter from 'gray-matter'
-export default function PostPage() {
-  return <div></div>
-}
+import marked from 'marked'
+import Layout from '../../component/Layout'
+import CategoryLabel from '../../component/CategoryLabel'
 
-export async function getStaticPaths({
-  frontmatter: { title, category, data, cover_image, author, author_image },
+export default function PostPage({
+  frontmatter: { title, category, date, cover_image, author, author_image },
   content,
   slug,
 }) {
+  return (
+    <Layout title={title}>
+      <Link href='/blog'>Kembali</Link>
+      <div className='w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6'>
+        <div className='flex justify-between items-center mt-4'>
+          <h1 className='text-5xl mb-7'>{title}</h1>
+          <CategoryLabel>{category}</CategoryLabel>
+        </div>
+
+        <Image
+          src={cover_image}
+          alt=''
+          width='100%'
+          height='100%'
+          layout='responsive'
+          className='rounded'
+        />
+        <div className='flex justify-between items-center bg-gray-100 p-2 my-8'>
+          <div className='flex items-center'>
+            {/* <Image
+              src={author_image}
+              alt=''
+              layout='fill'
+              className='mx-4 w-10 h-10 object-cover-rounded-full hidden sm:block'
+            /> */}
+            <h3>{author}</h3>
+          </div>
+
+          <div className='mr-4'>{date}</div>
+        </div>
+
+        <div className='blog-text mt-2'>
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
   const files = fs.readdirSync(path.join('posts'))
 
   const paths = files.map((filename) => ({
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ export default function HomePage({ posts }) {
   return (
     <Layout>
       <h1 className='text-5xl border-b-4 p-5 font-bold'>Latest Post</h1>
-
       <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
         {posts.map((post, index) => (
           <Post key={index} post={post} />
