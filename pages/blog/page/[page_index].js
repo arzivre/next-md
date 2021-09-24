16c6b1dc388d464236767e4d3f424226c692eb35
Arzivre <sonyfauzi@outlook.co.id>
2021-09-24 19:06:52 +0700

    fetch Paginated post

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -6,7 +6,7 @@ import Layout from '../../../component/Layout'
 import Post from '../../../component/Post'
 import { sortByDate } from '../../../utils'
 import { POSTS_PER_PAGE } from '../../../config'
-export default function BlogPage({ posts }) {
+export default function BlogPage({ posts, numPages, currentPage }) {
   return (
     <Layout>
       <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
@@ -38,7 +38,9 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps() {
+export async function getStaticProps({ params }) {
+  const page = parseInt((params && params.page_index) || 1)
+
   const files = fs.readdirSync(path.join('posts'))
 
   const posts = files.map((filename) => {
@@ -56,10 +58,17 @@ export async function getStaticProps() {
       frontmatter,
     }
   })
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
+  const pageIndex = page - 1
+  const orderedPost = posts
+    .sort(sortByDate)
+    .slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE)
 
   return {
     props: {
-      posts: posts.sort(sortByDate),
+      posts: orderedPost,
+      numPages,
+      currentPage: page,
     },
   }
 }
