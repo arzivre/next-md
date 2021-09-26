fd18da6ae92f09b7ae13456ae116670119a1ae4b
Arzivre <sonyfauzi@outlook.co.id>
2021-09-26 18:29:53 +0700

    Category sidebar

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -6,18 +6,29 @@ import path from 'path'
 
 import Layout from '@/components/Layout'
 import Post from '@/components/Post'
+import CategoryList from '@/components/CategoryList'
+
 import { getPosts } from '@/lib/posts'
 
-export default function CategoryBlogPage({ posts, categoryName }) {
+export default function CategoryBlogPage({ posts, categoryName, categories }) {
   return (
     <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold'>
-        Post in {categoryName}
-      </h1>
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+      <div className='flex justify-between'>
+        <div className='w-3/4 mr-10'>
+          <h1 className='text-5xl border-b-4 p-5 font-bold'>
+            Post in {categoryName}
+          </h1>
+
+          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+        </div>
+
+        <div className='w-1/4'>
+          <CategoryList categories={categories} />
+        </div>
       </div>
     </Layout>
   )
@@ -50,6 +61,11 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { category_name } }) {
   const posts = getPosts()
 
+  //* Get categories for sidebar
+  const categories = posts.map((post) => post.frontmatter.category)
+  // @ts-ignore
+  const uniqueCategories = [...new Set(categories)]
+
   //* filter post by category
   const categoryPosts = posts.filter(
     (post) => post.frontmatter.category.toLowerCase() === category_name
@@ -59,6 +75,7 @@ export async function getStaticProps({ params: { category_name } }) {
     props: {
       posts: categoryPosts.slice(0, 6),
       categoryName: category_name,
+      categories: uniqueCategories,
     },
   }
 }
--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -2,24 +2,34 @@
 import React from 'react'
 import fs from 'fs'
 import path from 'path'
+
 import Layout from '@/components/Layout'
 import Post from '@/components/Post'
 import Pagination from '@/components/Pagination'
+import CategoryList from '@/components/CategoryList'
+
 import { POSTS_PER_PAGE } from '@/config/index'
 import { getPosts } from '@/lib/posts'
 
-export default function BlogPage({ posts, numPages, currentPage }) {
+export default function BlogPage({ posts, numPages, currentPage, categories }) {
   return (
     <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
+      <div className='flex justify-between'>
+        <div className='w-3/4 mr-10'>
+          <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
 
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+
+          <Pagination currentPage={currentPage} numPages={numPages} />
+        </div>
+        <div className='w-1/4'>
+          <CategoryList categories={categories} />
+        </div>
       </div>
-
-      <Pagination currentPage={currentPage} numPages={numPages} />
     </Layout>
   )
 }
@@ -49,6 +59,11 @@ export async function getStaticProps({ params }) {
 
   const posts = getPosts()
 
+  //* Get categories for sidebar
+  const categories = posts.map((post) => post.frontmatter.category)
+  // @ts-ignore
+  const uniqueCategories = [...new Set(categories)]
+
   const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
   const pageIndex = page - 1
   const orderedPost = posts.slice(
@@ -61,6 +76,7 @@ export async function getStaticProps({ params }) {
       posts: orderedPost,
       numPages,
       currentPage: page,
+      categories: uniqueCategories,
     },
   }
 }
